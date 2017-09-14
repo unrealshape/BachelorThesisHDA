f37328737421ba0b772e40156330132ea28a0f76
admin <admin@DesktopPC.vm.local>
2017-09-14 15:07:12 +0200

    firstpush

---
+++ src/main/start.java
@@ -0,0 +1,12 @@
+package main;
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Will Start the Application now
+		System.out.println("Starten");
+
+	}
+
+}
